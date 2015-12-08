7f5dcd4d8cfce7196d7b6c3f1af7d0ae533d09d6
UnaPibaGeek <root@localhost.localdomain>
2015-12-08 03:13:59 -0300

    Some changes

--- crozono.py
+++ crozono.py
@@ -12,7 +12,7 @@
   Licensed under the GNU General Public License Version 2 (GNU GPL v2),
         available at: http://www.gnu.org/licenses/gpl-2.0.txt
   
- --
+  Kiwi
 -------------------------------------------------------------------------
 
 """
